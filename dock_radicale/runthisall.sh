3b653d9f30cd91e3a92aeaa92e9ea8ec2fd22a22
jumax9 <juacrumar@lairen.eu>
2022-04-03 16:51:05 +0200

    initial commit

---
+++ dock_radicale/runthisall.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+source ../generic.sh
+
+IMAGE_NAME=dock_radicale
+DATA_FOLDER=${PWD}/radicale_data
+PORT=5467
+
+
+create_ssl() {
+    echo "Installing certbot"
+    sudo apt install python3-certbot
+    sudo certbot
+}
+
+renew_ssl() {
+    sudo certbot renew
+}
+
+prepare_folder() {
+    mkdir -p ${DATA_FOLDER}/calendars
+    # The root of docker has 165536 in the outside world
+    # radicale is then running as an unprivileged user inside docker
+    echo ".. changing permissions for ${DATA_FOLDER}/calendars"
+    sudo chown :docker_escritura -R ${DATA_FOLDER}/calendars
+    sudo chmod g+rw -R ${DATA_FOLDER}/calendars
+}
+
+create_user() {
+    if [[ ! -d ${DATA_FOLDER} ]]
+    then
+        prepare_folder
+    fi
+    touch ${DATA_FOLDER}/users
+    htpasswd ${DATA_FOLDER}/users $1
+}
+
+usage() {
+    echo "~$ ./runthisall.sh -cbr -sn -f -u USER"
+    echo "    -s creates ssl certificate"
+    echo "    -n renews ssl certificate"
+    echo "    -u create a new user"
+    echo "    -f prepare radicale folder (permissions and so)"
+    echo "    -c remove test container and image"
+    echo "    -b build the docker image"
+    echo "    -r run docker image"
+}
+
+while getopts 'fcsnu:br' flag
+do
+    case "${flag}" in
+        c) docker_full_clean ${IMAGE_NAME} test_radicale ;;
+        s) create_ssl ;;
+        n) renew_ssl ;;
+        f) prepare_folder ;;
+        u) create_user ${OPTARG} ;;
+        b) docker_build ${IMAGE_NAME} ;;
+        r) docker_run ${IMAGE_NAME} test_radicale -p $PORT -v ${DATA_FOLDER}:/mnt -i /bin/sh ;;
+        *) usage
+            exit 1 ;;
+    esac
+done
+
+if [[ $# == 0 ]]
+then
+    usage
+    exit -1
+fi
