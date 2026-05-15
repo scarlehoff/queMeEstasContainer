14aba4399e574883194d6f166da64ea0c77369e1
juacrumar <juacrumar@lairen.eu>
2026-05-15 12:18:50 +0200

    working version

--- dock_neovim/run_container.sh
+++ dock_neovim/run_container.sh
@@ -8,7 +8,7 @@ WORKDIR=/Volumes/csDisk/
 # Keep the image's default user so /home/juacrumar stays writable.
 container run --rm -it \
   --platform linux/arm64 \
+  --network none \
   --volume "$WORKDIR:/work" \
   --workdir /work \
   nvim-alpine-arm64 "$@"
-#   --network none \
