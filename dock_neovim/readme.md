13bb15252b6ed83d15b360ef105070db4af4a93e
juacrumar <juacrumar@lairen.eu>
2026-05-15 14:15:22 +0200

    update the run script

--- dock_neovim/readme.md
+++ dock_neovim/readme.md
@@ -1,1 +1,10 @@
 This container is a bit different from all the others, in that the goal is to use Apple's containers: https://github.com/apple/container to isolate `neovim` and all of their plugins from the rest of the system.
+
+The build script moves the vim/neovim configuration files into the Dockerfile context and installs it,
+then upon building it also installs all plugins and mason, etc.
+
+Then we have a run script, `./run_container.sh`, which is basically running the container with vim.
+
+```sh
+./run_container.sh file.py
+```
--- dock_neovim/run_container.sh
+++ dock_neovim/run_container.sh
@@ -1,14 +1,43 @@
 #!/usr/bin/env bash
+#
+# This script works as my version of neovim
+# It opens a container with neovim already installed and with all plugins pre-installed,
+# but with no access to the internet.
+# It mounts the current directory with a few extra checks:
+# 1) If it is under the $MAIN_DISK folder at all, it mounts all the way down to the root of $MAIN_DISK
+# 2) Otherwise, if it is a git repository, mount all the way up to the root of the repository
+# 3) Otherwise just the current directory.
+#
 set -euo pipefail
 
-# To build it: container build --platform linux/arm64 -t nvim-alpine-arm64
+MAIN_DISK=/Volumes/csDisk/
 
-WORKDIR=/Volumes/csDisk/
+# Take the actual path (symlinks resolved)
+HOST_PWD=$(pwd -P)
 
-# Keep the image's default user so /home/juacrumar stays writable.
+# Folder that will be mounted in the container defaulting to the current folder
+MOUNT_ROOT=${HOST_PWD}
+# but will be changed by the checks below
+
+# If the current HOST_PWD is under MAIN_DISK, just mount MAIN_DISK
+# else, check whether we are in a 
+case "$HOST_PWD/" in
+  "$MAIN_DISK"*) MOUNT_ROOT=$MAIN_DISK ;;
+  *)
+    # Check whether we are at a git repo and then mount from the root
+    if GIT_ROOT=$(git -C "$HOST_PWD" rev-parse --show-toplevel 2>/dev/null); then
+      MOUNT_ROOT=$GIT_ROOT
+    fi
+    ;;
+esac
+
+
+
+# Then, we mount MOUNT_ROOT and we set the working directory as HOST_PWD
+# so that we are at the "right place"
 container run --rm -it \
   --platform linux/arm64 \
   --network none \
-  --volume "$WORKDIR:/work" \
-  --workdir /work \
-  nvim-alpine-arm64 "$@"
+  --volume "$MOUNT_ROOT:$MOUNT_ROOT" \
+  --workdir "$HOST_PWD" \
+  nvim-alpine-arm64 nvim "$@"
