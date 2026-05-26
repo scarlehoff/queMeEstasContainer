a7b1f4f8311742bf6218a499aa94354eabcea74c
juacrumar <juacrumar@lairen.eu>
2026-05-26 16:26:24 +0200

    add tmux

--- apple_debian/build_container.sh
+++ apple_debian/build_container.sh
@@ -2,10 +2,11 @@
 set -euo pipefail # Stop on command errors, unset variables, and failed pipelines.
 
 # Clean up by running rm -rf when you arrive to EXIT
-trap 'rm -rf nvim_config vim' EXIT 
+trap 'rm -rf nvim_config vim tmux_config' EXIT 
 
 # we need -L because .config/nvim is also a link
 cp -RL "$HOME/.config/nvim" nvim_config 
+cp -RL "$HOME/.config/tmux" tmux_config 
 cp -RL "$HOME/.vim/"  vim
 
 container build \
